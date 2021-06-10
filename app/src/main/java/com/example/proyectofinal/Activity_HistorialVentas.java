e712581d19624a85d0a2f29260e7c4e2d43e0464
Charlie3701 <ctorres.ct04@gmail.com>
2021-06-09 20:55:33 -0500

    Realización de Actividad Historial Ventas

---
+++ app/src/main/java/com/example/proyectofinal/Activity_HistorialVentas.java
@@ -0,0 +1,14 @@
+package com.example.proyectofinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_HistorialVentas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__historial_ventas);
+    }
+}
