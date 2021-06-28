342ccdcecccb9c5cf7c11172eac80fc174128e02
Charlie3701 <ctorres.ct04@gmail.com>
2021-06-28 17:21:26 -0500

    Realización del Activity Login

--- app/src/main/java/com/example/proyectofinal/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.proyectofinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText usuario,contrasena;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        usuario = (EditText)findViewById(R.id.txtUsuario);
+        contrasena = (EditText)findViewById(R.id.txtContraseña);
+    }
+
+    public void Login(View view){
+        String usuario_txt = usuario.getText().toString();
+        String contrasena_txt = contrasena.getText().toString();
+
+        if(usuario_txt.equals("Alejandra")&& contrasena_txt.equals("123")){
+            Intent ingreso = new Intent(this, MenuActivity.class);
+            startActivity(ingreso);
+        }
+        else
+            Toast.makeText(this, "Usuario y/o Contraseña Incorrectos",Toast.LENGTH_SHORT).show();
     }
 }
