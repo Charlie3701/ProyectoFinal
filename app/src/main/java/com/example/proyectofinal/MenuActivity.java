885d353df4284c5a5fbb2057d0cf14969bdac0cd
Charlie3701 <ctorres.ct04@gmail.com>
2021-06-28 17:53:41 -0500

    Transiciones del Menu

--- app/src/main/java/com/example/proyectofinal/MenuActivity.java
+++ app/src/main/java/com/example/proyectofinal/MenuActivity.java
@@ -2,7 +2,9 @@ package com.example.proyectofinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void Seleccion(View view){
+        switch(view.getId()){
+            case R.id.realizarVenta:
+                Intent pantalla_ventas = new Intent(this, Activity_Ventas.class);
+                startActivity(pantalla_ventas);
+                break;
+            case R.id.verInventario:
+                Intent pantalla_inventario = new Intent(this, Activity_VerInventario.class);
+                startActivity(pantalla_inventario);
+                break;
+            case R.id.modificarInventario:
+                Intent pantalla_modificar = new Intent(this, Activity_ModificarInventario.class);
+                startActivity(pantalla_modificar);
+                break;
+            case R.id.historialVentas:
+                Intent pantalla_historial = new Intent(this, Activity_HistorialVentas.class);
+                startActivity(pantalla_historial);
+                break;
+        }
     }
 }
